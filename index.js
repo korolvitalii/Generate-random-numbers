a1295287d536fa5d8f94b01590ee75ed6fc6b0e4
korolvitalii <korolvitalik@gmail.com>
2021-12-12 09:46:43 +0100

    💣👊🏻🐼

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _app = _interopRequireDefault(require("./app"));
+
+require("../style.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _app.default)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBOztBQUNBOzs7O0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgYXBwIGZyb20gJy4vYXBwJztcbmltcG9ydCAnLi4vc3R5bGUuY3NzJztcblxuYXBwKCk7XG4iXX0=
