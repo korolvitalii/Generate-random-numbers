97227f342543d0e593d5c5b85131450dd7e2f73d
korolvitalii <korolvitalik@gmail.com>
2021-12-12 11:06:17 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,51 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/index.html',
+          to: `${__dirname}/dist/index.html`,
+        },
+        {
+          from: './src/style.css',
+          to: `${__dirname}/dist/style.css`,
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new HtmlMinimizerPlugin(), new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'production',
+};
+
 // Generated using webpack-cli http://github.com/webpack-cli
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
